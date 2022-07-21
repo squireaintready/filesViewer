9081e0f272f0f28119e0ba4943db802828df7784
Samuel Jo <jo.sunghak@gmail.com>
2022-07-21 17:06:20 -0400

    dev/first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [files, setFiles] = useState([]);
+
+  const addNewFile = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const text = e.target.result;
+      setFiles([...files, { key: uuidv4(), text: text }]);
+    };
+    reader.readAsText(e.target.files[0]);
+  };
+
+  const saveImportedFile = (e) => {
+    e.preventDefault();
+    addNewFile(e);
+  };
+
+  const showAllText = () => (
+    files.map(file => (
+      <div className='singleFile'>
+        <p className='singleFile__p'>{file.text}</p>
+      </div>
+    ))
+  )
+
+  // on each file, allow for the following features
+  // edit file
+  // delete file
+  // highlight file
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={saveImportedFile} />
+      <br />
+      {showAllText()}
     </div>
   );
 }
-
-export default App;
